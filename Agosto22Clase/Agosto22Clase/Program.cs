50bb0696f9baf9e3f73fdab4c26b52cfb50998f0
brayanhernandez14 <142675727+brayanhernandez14@users.noreply.github.com>
2023-08-22 20:36:36 -0500

    Configuracion inicial

---
+++ Agosto22Clase/Agosto22Clase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Agosto22Clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
